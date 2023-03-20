7322430972c1210ab6be422ddf950d73a8a505c4
liubomyr-vynn <104894042+liubomyr-vynn@users.noreply.github.com>
2023-03-20 06:25:14 +0000

    Deploying to gh-pages from @ liubomyr-vynn/goit-js-hw-09@0fd34a11fe11129de7afed7038ff4c0572ee8659 🚀

--- 01-color-switcher.e9b44b54.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");function n(){t.toggleAttribute("disabled"),e.toggleAttribute("disabled")}t.addEventListener("click",(function(){n(),timerId=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){n(),clearInterval(timerId)}));
-//# sourceMappingURL=01-color-switcher.e9b44b54.js.map
